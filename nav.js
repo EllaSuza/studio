6253ec236c461a9e63225cbf4da08e3c7556c8ed
EllaSuza <123415452+EllaSuza@users.noreply.github.com>
2026-04-17 12:24:48 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,14 +2,14 @@ class MyNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <nav>
-            <a href="/studio/#portfolioIndex">
+            <a href="/#portfolioIndex">
                 <div id="portfolio"><span></span>PORTFOLIO</div>
             </a>
-            <a href="/studio/#ellaPage">
+            <a href="/#ellaPage">
                 <div id="ella"><span style="transform:rotate(135deg);"></span>ELLA SUZANNE<span style="transform:rotate(-45deg);"></span></div>
 				<div id="ellaPhone">ES</div>
             </a>
-            <a href="/studio/#paintingIndex">
+            <a href="/#paintingIndex">
                 <div id="contact">PAINTINGS<span></span></div>
             </a>
         </nav>
